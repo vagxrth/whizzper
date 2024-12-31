9e97eb005d227b75d6692df9dc159f9534032bc5
Vagarth <vagarth419@gmail.com>
2024-12-31 23:08:36 +0530

    feat: integrate clerk for authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
@@ -15,13 +16,15 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
+    <ClerkProvider>
       <html lang="en">
         <body
           suppressHydrationWarning
           className={jakarta.className}
         >
-            {children}
+          {children}
         </body>
       </html>
+    </ClerkProvider>
   )
 }
