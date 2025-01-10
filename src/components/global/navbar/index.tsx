52112aad3938cfcdc6d854269910277bc7a336b0
Vagarth <vagarth419@gmail.com>
2025-01-10 23:03:27 +0530

    feat: create a sheet component

--- src/components/global/navbar/index.tsx
+++ src/components/global/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
 import { usePath } from '@/hooks/use-path'
 import React from 'react'
 
@@ -8,9 +9,15 @@ type Props = {
 const Navbar = ({ slug }: Props) => {
 
     const { page } = usePath();
-
+    const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
   return (
-    <div>Navbar</div>
+    currentPage && <div className='flex flex-col'>
+        <div className='flex gap-x-3 lg:gap-x-5 justify-end'>
+            <span className='lg:hidden flex items-center flex-1 gap-x-2'>
+                
+            </span>
+        </div>
+    </div>
   )
 }
 
