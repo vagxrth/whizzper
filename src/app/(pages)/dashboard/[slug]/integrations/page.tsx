17c10239dce6cd84369a4f79887273cdcb646535
Vagarth <vagarth419@gmail.com>
2025-01-14 23:21:45 +0530

    feat: create an integration page

---
+++ src/app/(pages)/dashboard/[slug]/integrations/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Page = () => {
+  return (
+    <div className='flex justify-center'>
+        <div className='flex flex-col w-full lg:w-8/12 gap-y-5'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Page
