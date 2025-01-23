223021caa6668b8c691ad461a1382b2400268485
Vagarth <vagarth419@gmail.com>
2025-01-23 23:19:06 +0530

    feat: create a trigger component

--- src/app/(pages)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(pages)/dashboard/[slug]/automations/[id]/page.tsx
@@ -1,4 +1,6 @@
+import Trigger from '@/components/global/automations/trigger'
 import AutomationBreadCrumb from '@/components/global/bread-crumbs/automations'
+import { Warning } from '@/icons'
 import React from 'react'
 
 type Props = {
@@ -9,6 +11,13 @@ const Page = ({ params }: Props) => {
   return (
     <div className='flex flex-col items-center gap-y-20'>
         <AutomationBreadCrumb id={params.id}/>
+        <div className='w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3'>
+            <div className='flex gap-x-2'>
+              <Warning />
+              Trigger?
+            </div>
+            <Trigger id={params.id}/>
+        </div>
     </div>
   )
 }
