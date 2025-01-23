08f11a570498c1e4bb734096f536b4d9b945a4c5
Vagarth <vagarth419@gmail.com>
2025-01-23 23:24:02 +0530

    fix: passing id props as a promise

--- src/app/(pages)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(pages)/dashboard/[slug]/automations/[id]/page.tsx
@@ -4,19 +4,22 @@ import { Warning } from '@/icons'
 import React from 'react'
 
 type Props = {
-    params: { id: string }
+  params: Promise<{ id: string }>
 }
 
-const Page = ({ params }: Props) => {
+async function Page({ params }: Props) {
+
+  const { id } = await params
+
   return (
     <div className='flex flex-col items-center gap-y-20'>
-        <AutomationBreadCrumb id={params.id}/>
+        <AutomationBreadCrumb id={ id }/>
         <div className='w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3'>
             <div className='flex gap-x-2'>
               <Warning />
               Trigger?
             </div>
-            <Trigger id={params.id}/>
+            <Trigger id={ id }/>
         </div>
     </div>
   )
