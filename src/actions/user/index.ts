abf6af9cf7c695b51b8f4da349d51e8e65794f6f
Vagarth <vagarth419@gmail.com>
2025-01-28 22:19:22 +0530

    feat: create function to refresh insta token

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -2,6 +2,8 @@
 
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import { findUser } from "./queries"
+import { refreshToken } from "@/lib/fetch"
 
 export const onCurrentUser = async () => {
     const user = await currentUser()
@@ -13,7 +15,19 @@ export const onCurrentUser = async () => {
 export const onBoardUser = async() => {
     const user = await onCurrentUser();
     try {
-        
+        const found = await findUser(user.id)
+        if (found) {
+            if (found.integrations.length > 0) {
+                const today = new Date();
+                const timeLeft = found.integrations[0].expiresAt?.getTime() - today.getTime()
+
+                const days = Math.round(timeLeft / ( 1000 * 3600 * 24 ))
+
+                if (days < 5) {
+                    const refresh = await refreshToken()
+                }
+            }
+        }
     } catch (error) {
         
     }
