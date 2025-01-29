f51cc1ec5cbb1c4c5c12629b85ab41be4c92d4b9
Vagarth <vagarth419@gmail.com>
2025-01-29 22:16:25 +0530

    feat: complete dashboard page

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { onBoardUser } from '@/actions/user'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 
@@ -6,9 +7,14 @@ const page = async() => {
 
   const user = await onBoardUser();
 
-  return (
-    <div>page</div>
-  )
+  if (!user) return redirect('/signin');
+
+  if (user.status === 200 || user.status === 201) {
+    return redirect(`dashboard/${user.data?.firstName}${user.data?.lastName}`)
+  }
+
+  return redirect('/signin')
+
 }
 
 export default page
