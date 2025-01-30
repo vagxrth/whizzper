1d53fd2662a1dd918c6b13a55a4306c9f5b7c9f1
Vagarth <vagarth419@gmail.com>
2025-01-31 00:30:16 +0530

    feat: create react query to fetch user profile

---
+++ src/react-query/prefetch.ts
@@ -0,0 +1,18 @@
+import { onUserInfo } from "@/actions/user";
+import { QueryClient, QueryFunction } from "@tanstack/react-query";
+
+const prefetch = async (
+    client: QueryClient,
+    action: QueryFunction,
+    key: string
+  ) => {
+    return await client.prefetchQuery({
+      queryKey: [key],
+      queryFn: action,
+      staleTime: 60000,
+    })
+}
+
+export const PrefetchUserProfile = async(client: QueryClient) => {
+    return await prefetch(client, onUserInfo, 'user-profile')
+}
