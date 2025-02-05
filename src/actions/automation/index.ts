a3bdd7081a0486d630c970bd17d1c456bcb9bd6c
Vagarth <vagarth419@gmail.com>
2025-02-05 15:03:18 +0530

    feat: create a react query provider

--- src/actions/automation/index.ts
+++ src/actions/automation/index.ts
@@ -7,9 +7,9 @@ export const createAutomations = async (id?: string) => {
     const user = await onCurrentUser()
     try {
       const create = await createAutomation(user.id, id)
-      if (create) return { status: 200, data: 'Automation created', res: create }
+      if (create) return { status: 200, data: 'Automation Created!', res: create }
   
-      return { status: 404, data: 'Oops! something went wrong' }
+      return { status: 404, data: 'Oops! Something Went Wrong!' }
     } catch (error) {
       return { status: 500, data: 'Internal server error' }
     }
