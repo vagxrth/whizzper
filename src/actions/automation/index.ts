d6301d2f95795b629672762e83fc0c0068b57397
Vagarth <vagarth419@gmail.com>
2025-02-05 14:49:21 +0530

    feat: create automation server actions for creating automations

--- src/actions/automation/index.ts
+++ src/actions/automation/index.ts
@@ -1,7 +1,19 @@
 'use server'
 
 import { onCurrentUser } from "../user"
-import { getAutomations } from "./queries"
+import { createAutomation, getAutomations } from "./queries"
+
+export const createAutomations = async (id?: string) => {
+    const user = await onCurrentUser()
+    try {
+      const create = await createAutomation(user.id, id)
+      if (create) return { status: 200, data: 'Automation created', res: create }
+  
+      return { status: 404, data: 'Oops! something went wrong' }
+    } catch (error) {
+      return { status: 500, data: 'Internal server error' }
+    }
+}
 
 export const getAllAutomations = async() => {
     const user = await onCurrentUser()
